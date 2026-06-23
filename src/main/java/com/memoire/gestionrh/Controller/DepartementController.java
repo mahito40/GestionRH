dad5e7b7349a43b1b663e3ac59e1d1897afbde77
mahito40 <imanegbadamassi@gmail.com>
2026-06-23 11:05:47 +0100

    mise a jour

--- src/main/java/com/memoire/gestionrh/Controller/DepartementController.java
+++ src/main/java/com/memoire/gestionrh/Controller/DepartementController.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 public class DepartementController {
 
    
-    private DepartementService departementService;
+    private final DepartementService departementService;
 
     @PostMapping
     @PreAuthorize("hasAnyRole('RH','ADMIN')")
